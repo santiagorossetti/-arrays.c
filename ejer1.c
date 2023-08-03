5d72849aed6d6fd16daef33b3d7e090c008ae914
Santiago Rossetti Iñiguez <130598026+santiagorossetti@users.noreply.github.com>
2023-08-03 08:54:36 -0300

    Add files via upload

---
+++ ejer1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int papu [10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 10; i++){
+        
+        printf("%d \n" , papu[i]);
+        
+    }
+
+    return 0;
+}
