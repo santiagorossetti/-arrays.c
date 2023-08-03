0eef3958ef5812664a5c17d1591a13dff694cc72
Santiago Rossetti Iñiguez <130598026+santiagorossetti@users.noreply.github.com>
2023-08-03 10:03:11 -0300

    Add files via upload

---
+++ ejer3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (int argc, char *argv[]) {
+    srand(time(NULL));
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    int a[n];
+
+    for (int i = 0;i < n; i++) {
+        a[i] = rand () % m;
+        printf ("%d \n", a[i]);
+    }
+}
+
